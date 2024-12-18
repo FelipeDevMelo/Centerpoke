2a7ede8c458f32e27d5206a656929be552a8b342
CarlaMSLopes <carlalopes613@outlook.com>
2024-12-18 00:22:43 -0300

    :truck: chore: caminhos

--- app/(landingPage)/(home)/page.tsx
+++ app/(landingPage)/(home)/page.tsx
@@ -1,9 +1,9 @@
-import { Header } from "./Components/Header/Header";
+
 
 export default function Home() {
   return (
     <div className="mx-10 mt-3 ">
-      <Header />
+      
     </div>
   );
 }
---
+++ app/(landingPage)/PokeCenters/Page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const PokeCenters = () => {
+  return (
+    <div>Poke centers</div>
+  )
+}
