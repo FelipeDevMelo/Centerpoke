d42bddb14027c14daef453315e67b80ee9f31b1a
Felipe <luizfelipemelo001@gmail.com>
2024-12-18 15:15:38 -0300

    :bug: Fix: navbar paths

--- app/(landingPage)/_components/navigationNavbar.tsx
+++ app/(landingPage)/_components/navigationNavbar.tsx
@@ -1,44 +1,44 @@
-'use client'
+"use client";
 import { usePathname } from "next/navigation";
-import React from 'react'
+import React from "react";
 import Link from "next/link";
 
-export function NavigationNavbar  ()  {
-    const currentPath = usePathname();
-    const buttons: NavigationNavbar[] = [
-        {
-            path: "/plans",
-            label: "Insurance Plans"
-        },
+type NavigationNavbar = {
+  path: `/${string}`;
+  label: string;
+};
+export function NavigationNavbar() {
+  const currentPath = usePathname();
+  const buttons: NavigationNavbar[] = [
+    {
+      path: "/Plans",
+      label: "Insurance Plans",
+    },
 
-        {
-            path: "/pokeCenters",
-            label: "Poke Centers"
-        },
+    {
+      path: "/PokeCenters",
+      label: "Poke Centers",
+    },
 
-        {
-            path: "/company",
-            label: "Company"
-        },
+    {
+      path: "/Company",
+      label: "Company",
+    },
 
-        {
-            path: "/help",
-            label: "Help"
-        }
-    ]
-    return (
-        <div>
-            {buttons.map((button, key) => {
-
-                return (
-                    <Link
-                        key={key}
-                        href={button.path}
-                    >
-                       <h1>{button.label}</h1> 
-                    </Link>
-                )
-            })}
-        </div>
-    )
+    {
+      path: "/Help",
+      label: "Help",
+    },
+  ];
+  return (
+    <div>
+      {buttons.map((button, key) => {
+        return (
+          <Link key={key} href={button.path}>
+            <h1>{button.label}</h1>
+          </Link>
+        );
+      })}
+    </div>
+  );
 }
